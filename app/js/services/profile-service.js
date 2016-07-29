1668a78bc4baed07ef064b62d31861075813e55b
Aaron Krause <aaronjkrause@gmail.com>
2016-07-29 10:25:20 -0700

    project comments

--- app/js/services/profile-service.js
+++ app/js/services/profile-service.js
@@ -16,6 +16,10 @@ module.exports = function(app) {
       })
       .then((res) => {
         service.profile = res.data;
+        //this could potentially crash your stuff if there's no function passed into
+        //getProfile. Even if you know you're the one using it it's a good idea to check
+        //for that. You could use an if() or just cb && cb(). You could also catch the error
+        //with a catch chained onto this then but it's probably better just to check sooner.
         cb();
       }, ErrorService.logError('Error on profile'));
     };
